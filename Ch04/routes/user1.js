87654816e721da4b58b3d2ed3233c9b85fe82590
mh990407 <dlwlzjffj1@naver.com>
2023-05-25 14:17:06 +0900

    2023/05/25 mongoDB 리스트 출력/수정/삭제 실습

--- Ch04/routes/user1.js
+++ Ch04/routes/user1.js
@@ -3,12 +3,41 @@ const User1 = require("../schemas/user1");
 const router = express.Router();
 
 router.get("/list", async (req, res) => {
-  const data = await User1.find({ uid: req.body });
-  res.render("user1/list", data);
+  const users = await User1.find();
+
+  console.log(users);
+
+  res.render("user1/list", { users });
 });
 
 router.get("/register", (req, res) => {
   res.render("user1/register");
+});
+
+router.get("/modify", async (req, res) => {
+  const _id = req.query._id;
+  console.log("_id" + _id);
+
+  const user = await User1.findById(_id);
+  console.log("user" + user);
+
+  res.render("user1/modify", { user });
+});
+
+router.post("/modify", async (req, res) => {
+  const { _id, uid, name, hp, age } = req.body;
+
+  await User1.findByIdAndUpdate(_id, { name, hp, age });
+
+  res.redirect("/user1/list");
+});
+
+router.post("/delete", async (req, res) => {
+  const user = req.query._id;
+
+  await User1.deleteOne({ user });
+
+  res.redirect("/user1/list");
 });
 
 router.post("/register", async (req, res) => {
